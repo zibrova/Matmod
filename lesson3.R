67db142957149a00cc11dc40eacd9f8585bca154
unknown <uly061193@mail.ru>
2016-03-10 14:42:47 +0300

    njdfksfnlksj

--- lesson3.R
+++ lesson3.R
@@ -1,69 +1,13 @@
-#Циклы
-my_factorial=function(x){
-  
-  if(x>0){  
-  
-    factorial=1
-    for(n in 1:x){
-    factorial*n -> factorial 
-    }
-    return(factorial)
+dna
+dna=rep(c("A", "T", "G", "C"), c("250"))
+length(dna)
+sample(dna)
+length(dna)
+dna_AT=factor(dna)
+dna_AT= sample(c("A", "T", "G", "C"), size=1000, replace=TRUE)
+dna_AT[dna_AT=="A" | dna_AT=="T"]
 
-  }
-  else{
-    return (print ("oшибка"))
-  }
+dna_AT=="A"
+dna_AT=="T"
+length(dna_AT[dna_AT=="A" | dna_AT=="T"])
 
-}
-my_factorial(-5)
-
-
-
-
-
-#Графики
-
-mtcars
-diamonds
-ggplot(mtcars,aes(x=cyl, y=hp)) +
-  geom_point()+
-  geom_smooth()
-
-ggplot(mtcars,aes(x=cyl, y=hp, col=wt)) +
-  geom_point()+
-  geom_smooth()
-
-
-ggplot(mtcars, aes(cyl)) +
-  geom_bar()
-
-ggplot(mtcars,aes(x=cyl, y=hp, size=carb)) +
-  geom_point()+
-  geom_smooth()
-
-mpg
-ggplot(mpg,aes(x=class, y=hwy)) +
-  geom_point()+
-  geom_smooth()
-
-graph = ggplot(mpg,aes(x=class, y=hwy)) 
-graph + geom_point()
-graph + geom_boxplot()
-
-
-iris
-ggplot(iris,aes(x=Petal.Length, y=Sepal.Length)) +
-  geom_smooth()
-
-ggplot(iris,aes(x=Petal.Length, y=Sepal.Length, col=Species)) +
-  geom_point()
-
-
-eddy=read.csv("eddypro.csv", skip=1, sep=";")
-
-names(eddypro)=eddypro[2,]
-x=1:10
-x[-5]
-eddy=eddy[-1,]
-
-write.csv(eddy,"eddy2.csv")
